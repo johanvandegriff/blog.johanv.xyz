d24e8b5ed888e49e47a249c3a19aa9b91a98d10f
Johan Vandegriff <johan@vandymail.com>
2019-07-24 02:47:14 -0400

    changed path in sed command

--- patch.sh
+++ patch.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sed -i 's,<nav>$,<nav><a class="pinned" href="https://johanv.xyz/">Home</a>,' /go/templates/collection-post.tmpl /go/templates/collection.tmpl
+sed -i 's,<nav>$,<nav><a class="pinned" href="https://johanv.xyz/">Home</a>,' templates/collection-post.tmpl templates/collection.tmpl
