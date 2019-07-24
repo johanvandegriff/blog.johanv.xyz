f55295ee9b0574369ca28467ac77c4008d92d45e
Johan Vandegriff <johan@vandymail.com>
2019-07-24 03:09:36 -0400

    removed menu

--- patch.sh
+++ patch.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 sed -i 's,<nav>$,<nav><a class="pinned" href="https://johanv.xyz/">Home</a>,' templates/collection-post.tmpl templates/collection.tmpl
+sed -i 's,{{if or .IsOwner .SingleUser}}<nav id="manage"><ul>,{{if or .IsOwner}}<nav id="manage"><ul>,' templates/collection.tmpl
