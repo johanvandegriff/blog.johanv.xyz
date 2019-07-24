67dcfd12da3ec6603caaaea229e9c961b4f2efd0
Johan Vandegriff <johan@vandymail.com>
2019-07-24 02:52:07 -0400

    moved patch to after installation

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,5 @@ COPY patch.sh .
 
 EXPOSE 8080
 
-CMD ["./patch.sh"]
 CMD ["bin/writefreely"]
+CMD ["./patch.sh"]
--- patch.sh
+++ patch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sed -i 's,<nav>$,<nav><a class="pinned" href="https://johanv.xyz/">Home</a>,' templates/collection-post.tmpl templates/collection.tmpl
 ls > tmp123
-pwd > tmp123
-echo > tmp123
-find > tmp123
+pwd >> tmp123
+echo >> tmp123
+find >> tmp123
